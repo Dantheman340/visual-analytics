aa4208567db449da4700c39be165b78c47883080
Daniel Joensen <dantheman340@gmail.com>
2016-04-24 14:01:39 -0400

    parallell coordinates files, as well as created new sub directories

--- para_coords.js
+++ para_coords.js
@@ -1,3 +1,17 @@
 /**
- * Created by Millie on 4/24/2016.
+ * Created by Dan J. on 4/24/2016.
  */
+
+d3.csv("data/BaseballData.csv", function(error, data) {
+    if (error) throw error;
+
+    data.forEach(function(d) {
+        d.Abbrev = d.Abbrev;
+        d.Year= +d.Year;
+        d.HR= +d.HR;
+        d.SO= +d.SO;
+        d.OBP= +d.OBP;
+        d.SLG= +d.SLG;
+        d.FIP = +d.FIP;
+    });
+});
