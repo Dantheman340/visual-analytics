fdaab1f3f4786dd38c7817dae38cecc244474f7e
Daniel <s7_Cicatriz@yahoo.com>
2016-04-28 06:34:31 -0400

    Gradient specific year filter working, however need to get the dropdown functioning still.

--- para_coords.js
+++ para_coords.js
@@ -3,25 +3,45 @@
  */
 
 var color_scale = d3.scale.linear()
-    .domain([9, 50])
+    // .domain([data.min([g_data.HR]), data.max([g_data.HR])])
+    .domain([100,250])
     .range(["red", "green"])
     .interpolate(d3.interpolateLab);
 
 var pc1;
 var g_data;
+var selected_year = 2015;
 
 d3.csv('data/BaseballData_ParaCoords.csv', function(data) {
 
+    //data conversion from string to number
+    data.forEach(function(d) {
+        d.Year = +d.Year;
+        d.Payroll = +d.Payroll;
+        d.HR = +d.HR;
+        d.SO = +d.SO;
+        d.OBP = +d.OBP;
+        d.SLG = +d.SLG;
+        d.pFIP = +d.pFIP;
+        });
+
     g_data = data;
+//    console.log(g_data[0].Year);
+
     pc1 = d3.parcoords()("#para_coords")
-        .data(data)
+        .data(data.filter(function(d){return d.Year == selected_year}))
         .hideAxis(["Year","PayKey","Payroll","Abbrev"])
         .composite("darken")
-        .color(function(d) { return color_scale(d['Payroll']); })  // quantitative color scale
+        .color(function(d) { return color_scale(d["HR"]); })  // quantitative color scale
         .alpha(0.35)
         .render()
         .brushMode("1D-axes")  // enable brushing
         .interactive()  // command line mode
+
+    d3.select("#team-list")
+        .data(data)
+        .enter()
+        .append("table")
 
     var explore_count = 0;
     var exploring = {};
